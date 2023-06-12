a0447e0c4c0a7381079085506f8618530c3f0964
Alexander Mamlygo <alexander.mamlygo@gmail.com>
2023-06-12 20:27:10 +0400

    Initial Commit

---
+++ UIKit_programatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_programatically
+//
+//  Created by Aleksandr Mamlygo on 12.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
